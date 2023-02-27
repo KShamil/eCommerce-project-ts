f25f15c317ba8f9e7fdb2b2e3df7b279854aaa62
Shamil Kurbanov <107269680+KShamil@users.noreply.github.com>
2023-02-28 01:13:44 +0400

    run build

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -1,19 +1,19 @@
-// import { initializeApp } from "firebase/app";
-// // import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 
-// export const firebasecConfig = {
-//   apiKey: "AIzaSyBnkWI6DdIHa8-A9NDdjJ3EhFb84KS40Q8",
-//   authDomain: "applestore-ecommerce-project.firebaseapp.com",
-//   databaseURL: "https://applestore-ecommerce-project-default-rtdb.firebaseio.com",
-//   projectId: "applestore-ecommerce-project",
-//   storageBucket: "applestore-ecommerce-project.appspot.com",
-//   messagingSenderId: "1007005671152",
-//   appId: "1:1007005671152:web:e40513058fe7074fe22c35",
-// };
+export const firebasecConfig = {
+  apiKey: "AIzaSyBnkWI6DdIHa8-A9NDdjJ3EhFb84KS40Q8",
+  authDomain: "applestore-ecommerce-project.firebaseapp.com",
+  databaseURL: "https://applestore-ecommerce-project-default-rtdb.firebaseio.com",
+  projectId: "applestore-ecommerce-project",
+  storageBucket: "applestore-ecommerce-project.appspot.com",
+  messagingSenderId: "1007005671152",
+  appId: "1:1007005671152:web:e40513058fe7074fe22c35",
+};
 
 
-// const app = initializeApp(firebasecConfig);
-// export const db = getFirestore(app)
+const app = initializeApp(firebasecConfig);
+export const db = getFirestore(app)
 
-// export default app;
+export default app;
